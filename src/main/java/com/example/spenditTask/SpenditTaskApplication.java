286faea8c4ef341741d16227517f2ef0c2c108b7
Bessem11 <bessem.mnif@ensi-uma.tn>
2021-03-24 15:29:53 +0100

    initialize the project

---
+++ src/main/java/com/example/spenditTask/SpenditTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.spenditTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpenditTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpenditTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spenditTask/SpenditTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spenditTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpenditTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
